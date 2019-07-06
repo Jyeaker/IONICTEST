49ad314456667c293c458812d84fa15b06cc0dd1
Jyeaker <jomponindy@gmail.com>
2019-07-06 18:33:27 +0700

    new

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,18 @@ import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { NgxGaugeModule } from 'ngx-gauge';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,NgxGaugeModule],
   providers: [
     StatusBar,
-    SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    SplashScreen
+  ,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
 })
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,41 @@
 import { Component } from '@angular/core';
-
+declare var microgear;
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
+public temp : any="25";
+public hum : any="80%";
+public status : boolean= false ;
 
-  constructor() {}
 
+
+public _InitMicrogear(){
+  microgear.on('connected', () =>{
+	console.log("เชื่อมต่อสำเร็จ");
+  });
+  microgear.on('present', (event:any) =>{
+		console.log(event);
+	});
+
+	microgear.on('absent', (event:any)=> {
+		console.log(event);
+	});
+
+}
+
+
+
+
+  constructor() {
+    this._InitMicrogear();
+  }
+
+
+  public getTime (time){
+    let dt = new Date(time)
+    console.log(dt.getHours()+ " " + dt.getMinutes());
+  }
 }
